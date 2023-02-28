4ebed177988b5bd703c50dfa1372cf0d7a096e34
davidecardelli <davide.cardelli.94@gmail.com>
2023-02-28 17:06:54 +0100

    creo e lancio il seeder

--- database/seeders/MovieSeeder.php
+++ database/seeders/MovieSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Movie;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,21 @@ class MovieSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $movies = config('movies');
+
+        foreach ($movies as $movie) {
+            $new_movie = new Movie();
+            $new_movie->title = $movie['title'];
+            $new_movie->description = $movie['description'];
+            $new_movie->thumb = $movie['thumb'];
+            $new_movie->price = str_replace('$', '', $movie['price']);
+            $new_movie->title = $movie['title'];
+            $new_movie->series = $movie['series'];
+            $new_movie->sale_date = $movie['sale_date'];
+            $new_movie->type = $movie['type'];
+            $new_movie->artists = implode(',', $movie['artists']);
+            $new_movie->writers = implode(',', $movie['writers']);
+            $new_movie->save();
+        }
     }
 }
