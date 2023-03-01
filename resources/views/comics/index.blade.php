e78e4e2bcf95bb13f1de78ff4cd37db5a8069226
davidecardelli <davide.cardelli.94@gmail.com>
2023-03-01 21:22:00 +0100

    transform card and main form scss to bootstrap

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,10 +2,12 @@
 
 @section('content')
     <main>
-        <div class="container main-content">
-            @foreach ($comics as $comic)
-                @include('includes.card')
-            @endforeach
+        <div class="container">
+            <div class="row row-cols-6">
+                @foreach ($comics as $comic)
+                    @include('includes.card')
+                @endforeach
+            </div>
         </div>
     </main>
 @endsection
--- resources/views/includes/card.blade.php
+++ resources/views/includes/card.blade.php
@@ -1,6 +1,6 @@
-<div class="card">
+<div class="col">
     <a href="{{ route('comics.show', $comic['id']) }}">
         <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
     </a>
-    <h4>{{ $comic['title'] }}</h4>
+    <p class="text-center text-white mt-2 fw-bold text-uppercase">{{ $comic['title'] }}</p>
 </div>
