a10901f60b8840a8de970d262df46869a156b8f9
davidecardelli <davide.cardelli.94@gmail.com>
2023-03-01 19:30:01 +0100

    fixed home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <main>
         <div class="container">
-            <h1>Videos</h1>
+            <h1>Home</h1>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, dolorem ipsum reprehenderit quis nobis dolores,
                 impedit, minus veritatis pariatur delectus laboriosam nostrum consequatur ipsa eum est excepturi rem
                 expedita
